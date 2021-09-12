e67f84f1fe563fd6a972286c2ba559a95408613b
Tariq Abdulghani <tariq.abdulghani@idemia.com>
2021-09-12 15:32:43 +0200

    life cycle events

--- src/main/java/com/example/dependencyinjection/controllers/I18nController.java
+++ src/main/java/com/example/dependencyinjection/controllers/I18nController.java
@@ -1,11 +1,22 @@
 package com.example.dependencyinjection.controllers;
 
 import com.example.dependencyinjection.services.Greetings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Controller;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Controller
-public class I18nController {
+public class I18nController implements BeanFactoryAware, ApplicationContextAware {
+    private final Logger LOGGER = LoggerFactory.getLogger(I18nController.class);
     private final Greetings greetingsService;
 
     public I18nController(@Qualifier("i18nService") Greetings greetingsService) {
@@ -14,5 +25,25 @@ public class I18nController {
 
     public String sayHello() {
         return greetingsService.greet();
+    }
+
+    @PostConstruct
+    void afterInit(){
+        LOGGER.warn("......................post construct is called......................");
+    }
+
+    @PreDestroy
+    void beforeDestruction(){
+        LOGGER.warn("....................pre Destroy is called..........................");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        LOGGER.warn(".................... been Factory aware..........................");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        LOGGER.warn(".................... Application context aware ..........................");
     }
 }
