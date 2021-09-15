0561b29e4019899c23eabf186ead9ec890516b33
Tariq Abdulghani <tariq.abdulghani@idemia.com>
2021-09-15 10:12:40 +0200

    use of factory , learn about injection point

--- src/main/java/com/example/dependencyinjection/config/LoggerConfig.java
+++ src/main/java/com/example/dependencyinjection/config/LoggerConfig.java
@@ -2,6 +2,7 @@ package com.example.dependencyinjection.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,8 +10,9 @@ import org.springframework.context.annotation.Configuration;
 public class LoggerConfig {
 	
 	@Bean
-	Logger logger() {
-		return LoggerFactory.getLogger(LoggerConfig.class);
+	Logger logger(InjectionPoint injectionPoint) {
+//		System.out.println("#######################injection point: " + injectionPoint.getMember().getDeclaringClass().getSimpleName());
+		return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass().getSimpleName()); // using factory design pattern
 	}
 	
 }
